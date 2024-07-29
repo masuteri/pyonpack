136c1cac05125814c7b16fd62182d4ecd18be0be
masuteri <masuteri@proton.me>
2024-07-29 13:39:07 +0300

    update 0.6.0 and issue templates

---
+++ kubejs/server_scripts/fixMismatches.js
@@ -0,0 +1,25 @@
+ServerEvents.tags('item', event => {
+    event.add('c:raw_ores', 'tconstruct:raw_cobalt');
+    event.add('c:raw_cobalt_ores', 'tconstruct:raw_cobalt');
+    event.add('c:raw_cobalt_blocks', 'tconstruct:raw_cobalt_block');
+    event.add('c:storage_blocks', 'tconstruct:raw_cobalt_block');
+
+    const gems = ['emerald', 'diamond', 'quartz'];
+    gems.forEach(g => event.add('c:' + g + '_gems', 'minecraft:' + g));
+
+    const metals = [
+        'iron',
+        'gold',
+        'copper',
+        'cobalt',
+        'slimesteel',
+        'amethyst_bronze',
+        'rose_gold',
+        'pig_iron',
+        'manyullyn',
+        'hepatizon',
+        'queens_slime',
+        'netherite',
+    ];
+    metals.forEach(m => event.add('c:nuggets/' + m, '#c:' + m + '_nuggets'));
+});
